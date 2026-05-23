02c788b04469d905535fc84a4b4e61128b0fb6fe
amitkp <at.home.amit@gmail.com>
2026-05-23 14:21:32 +0530

    .

--- test/10_mmap.c
+++ test/10_mmap.c
@@ -7,12 +7,11 @@
 int main(void) {
   M(int *const,foo,); // declare safe type foo[][1]
 
-  // map foo -> foo[12][1] on-heap; NOTE: MAP_FAILED sanity checked already performed internally 
-  M(mmap(NULL, 48, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) , foo, 12);
+  // map foo -> foo[12][1] on-heap; NOTE: MAP_FAILED sanity check already included 
+  M(mmap(NULL, 48, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0), foo, 12);
 
   m(foo,5) = 10; // define foo[5][0]
 
-  printf("size of a memstruct should be 4. check: %zu\n", sizeof(foo));
   printf("var[5], with checks: %d\n", m(foo,5)); // fetch memory (with checks)
   printf("var[5], w/o checks: %d\n", m(foo)[5]); // fetch memory (no checks)
 
@@ -20,4 +19,7 @@ int main(void) {
   munmap(foo); // comment this to see leak warning
   return 0;
 }
-
+/*out
+var[5], with checks: 10
+var[5], w/o checks: 10
+*/

0698537c65952a668fb67a9cc08b87c7e6c9f488
amitkp <at.home.amit@gmail.com>
2026-05-30 14:03:10 +0530

    .

--- test/11_realloc.c
+++ test/11_realloc.c
@@ -6,21 +6,21 @@
 #include "../src/mstrct.h"
 
 int main(void) {
-  M(int *,foo,); // declare safe type int[][1]
-  M(int *,bar,); // declare safe type int[][1]
+  M(int *,foo,); // declare int[][1]
+  M(int *,bar,); // declare int[][1]
 
-  M(malloc(32), foo, 8);
-  M(mmap(NULL, 48, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0), bar, 12);
+  M(malloc(32), foo, 8); // int[8][1]
+  M(mmap(NULL, 48, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0), bar, 12); // int[12][1]
 
   m(foo,5) = 10; // define int[5][0]
-  m(bar,9) = 10; // define int[5][0]
+  m(bar,9) = 10; // define int[9][0]
 
-  printf("foo[5], before realloc: %d\n", m(foo,5)); // fetch memory (before realloc)
+  printf("foo[5], before realloc: %d\n", m(foo,5));   // fetch memory (before realloc)
   printf("bar[9], before realloc: %d\n\n", m(bar,9)); // fetch memory (before realloc)
 
   // re-allocation; note: M(foo) is implicitly cast to (void *) bas_addr
-  M(realloc(M(foo)->addr, 60), foo, 15);
-  M(mremap(M(bar)->addr, 48, 44, MREMAP_MAYMOVE), bar, 12);
+  M(realloc(M(foo)->addr, 60), foo, 15); // int[15][1]
+  M(mremap(M(bar)->addr, 48, 44, MREMAP_MAYMOVE), bar, 12); // int[12][1]
 
   printf("foo[5], after realloc: %d\n", m(foo,5)); // fetch memory (after realloc)
   printf("bar[9], after realloc: %d\n", m(bar,9)); // fetch memory (after realloc)
